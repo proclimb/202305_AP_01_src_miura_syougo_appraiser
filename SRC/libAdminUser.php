860825db8ad9a432d814d4d77f4e0b74ef1046cd
miura syogo <202305ap01_miura@x-ide.sakura.ne.jp>
2023-08-07 11:20:52 +0900

    #14484
    ・不具合状況
    ID　PASS　所属が必須なのに必須の文字が表示されない
    
    ・修正内容
    ID　PASS 　所属に必須文字を追加した

--- SRC/libAdminUser.php
+++ SRC/libAdminUser.php
@@ -111,7 +111,7 @@ function subAdminUserEdit()
 				<td><input type="text" name="password" value="<?php print $password; ?>" /></td>
 			</tr>
 			<tr>
-				<th>所属</th>
+				<th>所属<span class="red">（必須）</span></th>
 				<td><input type="radio" name="auth" value="0" checked="checked" /> <?php print fnAuthorityName(0); ?>
 					<input type="radio" name="auth" value="1" <?php if ($authority == 1) print ' checked="checked"'; ?> /> <?php print fnAuthorityName(1); ?>
 				</td>
